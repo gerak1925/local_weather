9af1a722c6cb2f627a796525171c93db6d04796e
gerak1925 <ikarous1925@gmail.com>
2016-06-20 20:28:42 +0300

    tidy code

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,27 @@
 var latitude, longitude;
 
-function geoFind() {
+function geoFind() 
+{
+
 	var output = document.getElementById("content");
 
-	if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
+	if (!navigator.geolocation) 
+	{
+    	output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    	return;
 	}
 
-	function success(position) {
-    latitude  = position.coords.latitude;
-    longitude = position.coords.longitude;
+	function success(position) 
+	{
+    	latitude  = position.coords.latitude;
+    	longitude = position.coords.longitude;
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-    output.innerHTML = '<p>api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=5b1a0c598f588ad14577a6cfc89433b2</p>';
-
-    // var img = new Image();
-    // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    // output.appendChild(img);
+    	output.innerHTML = '<p>api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=5b1a0c598f588ad14577a6cfc89433b2</p>';
 	};
 
-	function error() {
-    output.innerHTML = "Unable to retrieve your location";
+	function error() 
+	{
+    	output.innerHTML = "Unable to retrieve your location";
     };
 
     output.innerHTML = "<p>Locating…</p>";
