ecacbae8d56c98313e2d2382af2b92f4108bee47
gerak1925 <ikarous1925@gmail.com>
2016-07-07 20:04:26 +0300

    minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,52 @@
-$(document).ready(function()
+var latitude, longitude;
+
+function convertion()
 {
 
-	var latitude, longitude;
+}
 
-	function convertion()
+function processJSON(url)
+{
+	$.getJSON(url, function(jsonObj)
 	{
+		var data = JSON.parse(jsonObj);
+		var temper = parseFloat(data.main.temp) - 273.15;
 
+		$('#content').html("<p>" + temper.toString() + " <span id='unit'>Celsius</span></p>");
+		$('#content').append("<p>" + data.weather[0].description + "</p>");
+		$('#content').append("<p>" + data.main.humidity + " humidity</p>");
+	});
+}
+
+function geoFind()
+{
+	var output = document.getElementById("content");
+
+	if (!navigator.geolocation) 
+	{
+	    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+	    return;
 	}
 
-	function processJSON(url)
+	function success(position) 
 	{
-		$.getJSON(url, function(jsonObj)
-		{
-			var temper = parseFloat(jsonObj.main.temp) - 273.15;
+	    lat  = position.coords.latitude;
+	    lon = position.coords.longitude;
 
-			$('#content').html("<p>" + temper.toString() + " <span id="unit" onclick="conversion()">Celsius</span></p>");
-			$('#content').append("<p>" + jsonObj.weather[0].description + "</p>");
-			$('#content').append("<p>" + jsonObj.main.humidity + " humidity</p>");
-		}
-	}
+	    var url = 'api.openweathermap.org/data/2.5/weather?lat=' + lat.toString() + '&lon=' + lon.toString() + '&APPID=5b1a0c598f588ad14577a6cfc89433b2';
+	    $('#content').html('<p>' + url + '</p>');
 
-	function geoFind() 
+	    processJSON(url);
+	};
+
+	function error() 
 	{
-		var output = document.getElementById("content");
+	    output.innerHTML = "Unable to retrieve your location";
+	};
 
-		if (!navigator.geolocation) 
-		{
-	    	output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-	    	return;
-		}
+	output.innerHTML = "<p>Locating…</p>";
 
-		function success(position) 
-		{
-	    	lat  = position.coords.latitude;
-	    	lon = position.coords.longitude;
+	navigator.geolocation.getCurrentPosition(success, error);
+}
 
-	    	var url = 'api.openweathermap.org/data/2.5/weather?lat=' + lat.toString() + '&lon=' + lon.toString() + '&APPID=5b1a0c598f588ad14577a6cfc89433b2';
-
-	    	processJSON(url);
-		};
-
-		function error() 
-		{
-	    	output.innerHTML = "Unable to retrieve your location";
-	    };
-
-	    output.innerHTML = "<p>Locating…</p>";
-
-	    navigator.geolocation.getCurrentPosition(success, error);
-	}
-});
+$('button').on('click', geoFind());
