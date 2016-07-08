f534b9524404b5faffbeccc510a2e388a7f9502d
gerak1925 <ikarous1925@gmail.com>
2016-07-08 17:42:42 +0300

    several changes

--- scripts/script.js
+++ scripts/script.js
@@ -8,16 +8,23 @@ function convertion()
 
 function processJSON(url)
 {
-	$.getJSON(url, function(obj)
-	{
+	alert(url);
+	var jqxhr = $.getJSON(url, function(data) {
+		var obj = data;
 		var temper = parseFloat(obj.main.temp) - 273.15;
 		var weatherIcon = 'http://openweathermap.org/img/w/' + obj.weather[0].icon + '.png';
+
+		console.dir(obj);
 
 		$('#content').html('<p>' + temper.toString() + ' <span id="unit">Celsius</span></p>');
 		$('#content').append("<p>" + obj.weather[0].description + "</p>");
 		$('#content').append("<p>" + obj.main.humidity + " humidity</p>");
 		$('#content').append('<p><img src="' + weatherIcon + '" /></p>');
-	});
+	})
+		.fail(function( jqxhr, textStatus, error ) 
+			{ 
+				var err = textStatus + ", " + error; console.log( "Request Failed: " + err ); 
+			});
 }
 
 function geoFind()
@@ -40,6 +47,8 @@ function geoFind()
 	    url = "api.openweathermap.org/data/2.5/weather?lat=" + lat.toString() + "&lon=" + lon.toString() + "&APPID=5b1a0c598f588ad14577a6cfc89433b2";
 	    // $('#content').html('<p>' + url + '</p>'); This line is here just to check if the API link is ok.
 
+	    processJSON(url);
+
 	};
 
 	function error() 
@@ -53,4 +62,3 @@ function geoFind()
 }
 
 $('button').on('click',geoFind);
-processJSON(url);
