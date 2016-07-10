9d7f60ba16b44b776d43609c67070aa3ceaa6b8e
gerak1925 <ikarous1925@gmail.com>
2016-07-10 12:52:44 +0300

    Final functional-wise touches

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,27 @@
-var lat, lon;
+var lat, lon, celsius, fahren;
 var url = "";
 
-function convertion()
-{
-
-}
+String.prototype.capitalize = function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
 
 function processJSON(url)
 {
-	alert(url);
-	// var jqxhr = $.getJSON(url, function(data) {
-	// 	var obj = data;
-	// 	var temper = parseFloat(obj.main.temp) - 273.15;
-	// 	var weatherIcon = 'http://openweathermap.org/img/w/' + obj.weather[0].icon + '.png';
-
-	// 	console.dir(obj);
-
-	// 	$('#content').html('<p>' + temper.toString() + ' <span id="unit">Celsius</span></p>');
-	// 	$('#content').append("<p>" + obj.weather[0].description + "</p>");
-	// 	$('#content').append("<p>" + obj.main.humidity + " humidity</p>");
-	// 	$('#content').append('<p><img src="' + weatherIcon + '" /></p>');
-	// })
-	// 	.fail(function( jqxhr, textStatus, error ) 
-	// 		{ 
-	// 			var err = textStatus + ", " + error; console.log( "Request Failed: " + err ); 
-	// 		});
-
 	$.ajax({
 		type: "GET",
 		url: url,
 		dataType: "json",
 		success: function (obj) {
-			var temper = parseFloat(obj.main.temp) - 273.15;
+			celsius = Math.floor(parseFloat(obj.main.temp) - 273.15);
+			fahren = Math.floor((celsius * 1.8) + 32);
 			var weatherIcon = 'http://openweathermap.org/img/w/' + obj.weather[0].icon + '.png';
 
-			$('#content').html('<p>' + temper.toString() + ' <span id="unit">Celsius</span></p>');
-			$('#content').append("<p>" + obj.weather[0].description + "</p>");
-			$('#content').append("<p>" + obj.main.humidity + " humidity</p>");
-			$('#content').append('<p><img src="' + weatherIcon + '" /></p>');
+			$('#celsius').html('<p>' + celsius.toString() + '° Celsius</p>');
+			$('#fahrenheit').html('<p>' + fahren.toString() + '° Fahrenheit</p>');
+			$('#fahrenheit').hide();
+			$('#description').text(obj.weather[0].description.capitalize());
+			$('#humidity').text(obj.main.humidity + '% Humidity');
+			$('#weatherIcon').append('<img src="' + weatherIcon + '" />');
 		},
 		error: function (XMLHttpRequest, textStatus, errorThrown) {
 			alert("Error: " + errorThrown);
@@ -53,7 +37,7 @@ function geoFind()
 
 	if (!navigator.geolocation) 
 	{
-	    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+	    $('#celsius').innerHTML = "<p>Geolocation is not supported by your browser</p>";
 	    return;
 	}
 
@@ -62,21 +46,28 @@ function geoFind()
 	    lat  = position.coords.latitude;
 	    lon = position.coords.longitude;
 
-	    url = "api.openweathermap.org/data/2.5/weather?lat=" + lat.toString() + "&lon=" + lon.toString() + "&APPID=5b1a0c598f588ad14577a6cfc89433b2";
-	    // $('#content').html('<p>' + url + '</p>'); This line is here just to check if the API link is ok.
+	    url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat.toString() + "&lon=" + lon.toString() + "&APPID=5b1a0c598f588ad14577a6cfc89433b2";
 
 	    processJSON(url);
-
 	};
 
 	function error() 
 	{
-	    output.innerHTML = "Unable to retrieve your location";
+	    $('#celsius').innerHTML = "Unable to retrieve your location";
 	};
 
-	output.innerHTML = "<p>Locating…</p>";
+	$('#celsius').innerHTML = "<p>Locating…</p>";
 
 	navigator.geolocation.getCurrentPosition(success, error);
 }
 
 $('button').on('click',geoFind);
+
+$('#celsius').click(function(){
+	$('#celsius').hide();
+	$('#fahrenheit').show();
+});
+$('#fahrenheit').click(function(){
+	$('#fahrenheit').hide();
+	$('#celsius').show();
+});
